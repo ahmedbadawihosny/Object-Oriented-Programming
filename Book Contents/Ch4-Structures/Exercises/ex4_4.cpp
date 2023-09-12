fca1d8a15dfe3f999b8974b61553b9edc5289550
ahmed badawi <ahmedbad063@gmail.com>
2023-09-13 01:37:34 +0300

    upload ex4_4 file

--- Book Contents/Ch4-Structures/Exercises/ex4_4.cpp
+++ Book Contents/Ch4-Structures/Exercises/ex4_4.cpp
@@ -13,8 +13,11 @@ int main()
   
   cout<<"---------------------------------------------------\n";
   cout<<"Enter data for three employees (number,compensation):\n";
+  cout<<"Enter data for the first employee : ";
   cin>>employee1.employee_number>>employee1.employee_compensation;
+  cout<<"Enter data for the second employee : ";
   cin>>employee2.employee_number>>employee2.employee_compensation;
+  cout<<"Enter data for the third employee : ";
   cin>>employee3.employee_number>>employee3.employee_compensation;
 
   cout<<"---------------------------------------------------\n";
